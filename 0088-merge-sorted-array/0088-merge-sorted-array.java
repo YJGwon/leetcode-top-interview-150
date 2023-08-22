4fdc8ec06ea2fff312a45e4a56ddf8d6c4c7a0ed
Forky <trulyforky@gmail.com>
2023-08-22 23:39:45 +0900

    Time: 0 ms (100%), Space: 41.2 MB (80.35%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -4,22 +4,14 @@ class Solution {
             return;
         }
 
-        int index1 = m - 1;
-        int index2 = n - 1;
-        for (int i = m + n - 1; i >= 0; i--) {
-            if (index1 < 0) { // nums1 정렬 먼저 끝남
-                nums1[i] = nums2[index2--];
-                continue;
-            }
-            if (index2 < 0) { // nums2 정렬 먼저 끝남
-                nums1[i] = nums1[index1--];
-                continue;
-            }
-
-            if (nums1[index1] < nums2[index2]) {
-                nums1[i] = nums2[index2--];
+        int pointer1 = m - 1; // nums1의 정렬되지 않은 마지막 index
+        int pointer2 = n - 1; // nums2의 정렬되지 않은 마지막 index
+        int i = m + n - 1; // 저장할 index
+        while (pointer2 >= 0) {
+            if (pointer1 >= 0 && nums1[pointer1] > nums2[pointer2]) {
+                nums1[i--] = nums1[pointer1--];
             } else {
-                nums1[i] = nums1[index1--];
+                nums1[i--] = nums2[pointer2--];
             }
         }
     }
