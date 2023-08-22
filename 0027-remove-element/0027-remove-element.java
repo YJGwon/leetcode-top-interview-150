f1d81f7baccf96fe8defeafbd967616f5904693d
Forky <trulyforky@gmail.com>
2023-08-23 01:11:46 +0900

    Time: 0 ms (100%), Space: 40.5 MB (99.61%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -2,8 +2,6 @@ import java.util.Arrays;
 
 class Solution {
 
-    private static final int MAX_VALUE = 100;
-
     public int removeElement(int[] nums, int val) {
         int k = 0;
         for (int i = 0; i < nums.length; i++) {
