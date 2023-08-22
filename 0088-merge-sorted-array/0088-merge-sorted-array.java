99d805435fd06f8da7adad21db95623a2e2bf9e9
Forky <trulyforky@gmail.com>
2023-08-22 23:51:51 +0900

    Time: 0 ms (100%), Space: 40.8 MB (98.39%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,17 +1,13 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        if (n == 0) {
-            return;
-        }
-
         int pointer1 = m - 1; // nums1의 정렬되지 않은 마지막 index
         int pointer2 = n - 1; // nums2의 정렬되지 않은 마지막 index
         int i = m + n - 1; // 저장할 index
         while (pointer2 >= 0) {
-            if (pointer1 >= 0 && nums1[pointer1] > nums2[pointer2]) {
-                nums1[i--] = nums1[pointer1--];
-            } else {
+            if (pointer1 < 0 || nums1[pointer1] < nums2[pointer2]) {
                 nums1[i--] = nums2[pointer2--];
+            } else {
+                nums1[i--] = nums1[pointer1--];
             }
         }
     }
