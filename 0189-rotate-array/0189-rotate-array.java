fa89e14eda4df0c5572a5318f81788d509f11891
Forky <trulyforky@gmail.com>
2023-08-23 19:31:49 +0900

    Time: 0 ms (100%), Space: 56 MB (41.05%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,18 +1,18 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         k %= nums.length;
+        flip(nums, 0, nums.length - 1);
+        flip(nums, 0, k - 1);
+        flip(nums, k, nums.length - 1);
+    }
 
-        int[] temp = new int[k];
-        for (int i = 0; i < k; i++) {
-            temp[i] = nums[nums.length - k + i];
-        }
-
-        for (int i = nums.length - k - 1; i >= 0; i--) {
-            nums[i + k] = nums[i];
-        }
-
-        for (int i = 0; i < k; i++) {
-            nums[i] = temp[i];
+    private void flip(int[] arr, int startsAt, int endsAt) {
+        int left = startsAt;
+        int right = endsAt;
+        while (left < right) {
+            int temp = arr[left];
+            arr[left++] = arr[right];
+            arr[right--] = temp;
         }
     }
 }
