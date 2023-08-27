aea48e0124182c074745d50cdd3af86497ccbe50
Forky <trulyforky@gmail.com>
2023-08-27 13:27:27 +0900

    Time: 4 ms (14.96%), Space: 42.8 MB (98.4%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,30 @@
+import java.util.Set;
+import java.util.HashSet;
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+
+    public boolean hasCycle(ListNode head) {
+        final Set<ListNode> visitedNodes = new HashSet<>();
+
+        ListNode currentNode = head;
+        while (currentNode != null) {
+            if (visitedNodes.contains(currentNode)) {
+                return true;
+            }
+            visitedNodes.add(currentNode);
+            currentNode = currentNode.next;
+        }
+        return false;
+    }
+}
