8d804614cc238102a0108c22b4fc01355a1f492b
Forky <trulyforky@gmail.com>
2023-08-29 01:26:28 +0900

    Time: 0 ms (100%), Space: 40.9 MB (67.45%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,18 +1,38 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        final int m = matrix.length;
-        final int n = matrix[0].length;
+        final int r = findRow(matrix, target);
+        if (r < 0) {
+            return false;
+        }
+        return isTargetInRow(matrix[r], target);
+    }
+
+    private int findRow(int[][] matrix, int target) {
         int left = 0;
-        int right = m * n - 1;
+        int right = matrix.length - 1;
         while (left <= right) {
             final int mid = (left + right) / 2;
-            final int x = mid / n;
-            final int y = mid % n;
-            final int number = matrix[x][y];
-            if (number == target) {
+            if (matrix[mid][0] == target) {
+                return mid;
+            }
+            if (matrix[mid][0] < target) {
+                left = mid + 1;
+                continue;
+            }
+            right = mid - 1;
+        }
+        return right;
+    }
+
+    private boolean isTargetInRow(int[] row, int target) {
+        int left = 0;
+        int right = row.length - 1;
+        while (left <= right) {
+            final int mid = (left + right) / 2;
+            if (row[mid] == target) {
                 return true;
             }
-            if (number < target) {
+            if (row[mid] < target) {
                 left = mid + 1;
                 continue;
             }
