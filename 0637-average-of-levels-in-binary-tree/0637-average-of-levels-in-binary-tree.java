fef5fc36780c3fcbd50a32e60249618d9b2ec1ba
Yejin Gwon(Forky) <trulyforky@gmail.com>
2023-09-07 16:08:54 +0900

    Time: 4 ms (12.78%), Space: 44.3 MB (83.24%) - LeetHub

---
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        if (root == null) {
+            return List.of();
+        }
+
+        List<Double> answers = new ArrayList<>();
+        Queue<LeveledNode> queue = new ArrayDeque<>();
+        queue.offer(new LeveledNode(root, 0));
+
+        int level = 0;
+        double sum = 0;
+        double count = 0;
+        while(!queue.isEmpty()) {
+            LeveledNode now = queue.poll();
+
+            sum += now.node.val;
+            count++;
+
+            if (now.node.left != null) {
+                queue.offer(new LeveledNode(now.node.left, now.level + 1));
+            }
+
+            if (now.node.right != null) {
+                queue.offer(new LeveledNode(now.node.right, now.level + 1));
+            }
+
+            if (queue.isEmpty() || level != queue.peek().level) {
+                double avg = Math.floor((sum / count) * 1_000_000) / 1_000_000;
+                answers.add(avg);
+                level++;
+                sum = 0;
+                count = 0;
+            }
+        }
+
+        return answers;
+    }
+
+    private static class LeveledNode {
+        TreeNode node;
+        int level;
+
+        LeveledNode(TreeNode node, int level) {
+            this.node = node;
+            this.level = level;
+        }
+    }
+}
