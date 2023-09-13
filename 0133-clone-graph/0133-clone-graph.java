e66bd90e143e24c08542ea2de9bc38af87cd592a
Yejin Gwon(Forky) <trulyforky@gmail.com>
2023-09-13 20:54:36 +0900

    Time: 26 ms (64.36%), Space: 41.6 MB (80.76%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.java
@@ -0,0 +1,49 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+
+    private Map<Integer, Node> copiedNodes;
+
+    public Node cloneGraph(final Node node) {
+        if (node == null) {
+            return null;
+        }
+
+        copiedNodes = new HashMap<>();
+        return deepCopy(node);
+    }
+
+    private Node deepCopy(final Node node) {
+        if (copiedNodes.containsKey(node.val)) {
+            return copiedNodes.get(node.val);
+        }
+
+        final Node copiedNode = new Node(node.val);
+        copiedNodes.put(node.val, copiedNode);
+
+        final List<Node> copiedNeighbors = node.neighbors.stream()
+            .map(neighbor -> deepCopy(neighbor))
+            .collect(Collectors.toList());
+        copiedNode.neighbors = copiedNeighbors;
+
+        return copiedNode;
+    }
+}
